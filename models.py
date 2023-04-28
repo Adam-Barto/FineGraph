62211d12f52a4158d63eeb25eac450a80ff19931
Adam-Barto <adam.jbarto@gmail.com>
2023-04-28 14:36:12 -0400

    Swapped Enums to Dict for easier use with passed int values

--- models.py
+++ models.py
@@ -1,25 +1,21 @@
-from enum import Enum
 from datetime import datetime
 from marshmallow_sqlalchemy import fields
 from config import db, ma
 
-
-class TypeOfPayment(Enum):
-    CHECK = 1,
-    VISA = 2,
-    MASTERCARD = 3,
-
-    OTHER = 0  # When the person does not fill it out.
-
-
-class TypeOfCategory(Enum):
-    FOOD = 1,
-    HEALTH = 2,
-    EDUCATION = 3,
-    ENTERTAINMENT = 4,
-    CHARITY = 5,
-
-    OTHER = 0  # When the person does not fill it out.
+TypeOfPayment_Dict = {
+    0: 'Other',
+    1: 'Check',
+    2: 'Visa',
+    3: 'MasterCard'
+}
+TypeOfCategory_Dict = {
+    0: 'Other',
+    1: 'Food',
+    2: 'Health',
+    3: 'Education',
+    4: 'Entertainment',
+    5: 'Charity'
+}
 
 
 class DataPoint(db.Model):
@@ -28,7 +24,7 @@ class DataPoint(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
 
     # The type of payment method used.
-    payment_type = db.Column(db.Enum, nullable=False)
+    payment_type = db.Column(db.VARCHAR(32), nullable=False)
     # Can be last digits of the card or a check number
     payment_from = db.Column(db.Integer, nullable=False)
 
@@ -39,7 +35,7 @@ class DataPoint(db.Model):
     date = db.Column(db.DateTime, nullable=False)
 
     # Category of Payment
-    category = db.Column(db.Enum, nullable=False)
+    category = db.Column(db.VARCHAR(32), nullable=False)
 
 
 class DataSchema(ma.SQLAlchemyAutoSchema):
