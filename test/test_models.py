06c4369d5b4cf1280a02fc97615bf8c18267edef
Adam-Barto <adam.jbarto@gmail.com>
2023-04-29 14:22:37 -0400

    Made user Test Creation work if the database is empty, and also clean up after itself.

--- test/test_models.py
+++ test/test_models.py
@@ -21,22 +21,45 @@ class Test(TestCase):
             db.create_all()
             last_name = "Test"
             first_name = "User"
-            user_count = pd.DataFrame(users.read_all()).id.count()
             test_user = json.loads('{'+f'"last_name": "{last_name}", "first_name": "{first_name}", "datapoints": []'+'}'
-                                )
-            expected = user_count + 1
-            users.create(test_user)
-            actual = pd.DataFrame(users.read_all()).id.count()
+                                   )
+            new_user = users.create(test_user)
+            actual = users.read_one(new_user[0].get('id')).get('last_name')
+            expected = 'Test'
+            self.assertEqual(expected, actual)
+            users.delete(new_user[0].get('id'))
+
+    def test_datapoint_creation(self):
+        setup_test_database()
+        with app.app_context():
+            db.create_all()
+            payment_type = 'Visa'
+            payment_from = '1234'
+            amount = '123.45'
+            date = '02-03-2009'
+            category = 'Food'
+            test_datapoints = json.loads(
+                '{' + f'"payment_type": {payment_type}, "payment_from": {payment_from}, "amount": {amount}, "date": {date}, "category": {category}' + '}'
+            )
+            datapoint_count = pd.DataFrame(datapoints.read_data()).id.count()
+            expected = datapoint_count + 1
+            users.create(test_datapoints)
+            actual = pd.DataFrame(datapoints.read_all()).id.count()
             self.assertEqual(expected, actual)
 
-    def test_type_of_category(self):
-        self.fail()
+    def test_print_dataset(self):
+        setup_test_database()
+        with app.app_context():
+            db.create_all()
+            df = pd.DataFrame(users.read_all())
+            print(df)
+            # df.drop_duplicates(subset='last_name')
+            # db.drop_all()
 
-    def test_data_point(self):
-        self.fail()
-
-    def test_user(self):
-        self.fail()
+    def test_delete_database(self):
+        setup_test_database()
+        with app.app_context():
+            db.drop_all()
 
     def test_data_base(self):
         self.fail()
