d90c913bdea9103177f3559ed345ef19afcc5ee4
Adam-Barto <adam.jbarto@gmail.com>
2023-05-01 08:52:53 -0400

    Tests refined, with generative tests used during testing moved to the Troubleshooting script

--- test/test_models.py
+++ test/test_models.py
@@ -23,13 +23,14 @@ class Test(TestCase):
             db.create_all()
             last_name = "Test"
             first_name = "User"
-            test_user = json.loads('{'+f'"last_name": "{last_name}", "first_name": "{first_name}", "datapoints": []'+'}'
-                                   )
+            test_user = json.loads(
+                '{' + f'"last_name": "{last_name}", "first_name": "{first_name}", "datapoints": []' + '}'
+                )
             new_user = users.create(test_user)
             actual = users.read_one(new_user[0].get('id')).get('last_name')
             expected = 'Test'
             self.assertEqual(expected, actual)
-            users.delete(new_user[0].get('id')) # Clean up after itself.
+            users.delete(new_user[0].get('id'))  # Clean up after itself.
 
     def test_datapoint_creation(self):
         setup_test_database()
@@ -52,44 +53,12 @@ class Test(TestCase):
             actual = datapoints.read_data(new_datapoint[0].get('id')).get('payment_type')
             expected = 'Check'
             self.assertEqual(expected, actual)
-            # datapoints.delete(new_datapoint[0].get('id')) # Clean up after itself.
-
-    def test_print_dataset(self):
-        setup_test_database()
-        with app.app_context():
-            db.create_all()
-            df = pd.DataFrame(users.read_all())
-            print(df)
-    #         # df.drop_duplicates(subset='last_name')
-    #         # db.drop_all()
-    #
-    # def test_delete_database(self):
-    #     setup_test_database()
-    #     with app.app_context():
-    #         db.drop_all()
+            datapoints.delete(new_datapoint[0].get('id'))  # Clean up after itself.
 
     def test_graphs(self):
         setup_test_database()
         with app.app_context():
             db.create_all()
-            graph(1, 'date', 'Food')
-
-    def test_create_points(self):
-        setup_test_database()
-        with app.app_context():
-            db.create_all()
-            for i in range(100):
-                payment_type = TypeOfPayment_Dict.get(randint(0,3))
-                payment_from = f"{randint(1, 9)}{randint(0, 9)}{randint(0, 9)}{randint(0, 9)}"
-                amount = f"{randint(10, 50)}.{randint(00,99)}"
-                date = f"{randint(1, 12)}-{randint(1, 30)}-20{randint(0,10)}"
-                category = TypeOfCategory_Dict.get(randint(0, 5))
-                test_datapoint = json.loads(
-                    '{' + f'"user_id": "1",'
-                          f' "payment_type": "{payment_type}",'
-                          f' "payment_from": "{payment_from}",'
-                          f' "amount": "{amount}",'
-                          f' "date": "{date}",'
-                          f' "category": "{category}"' + '}'
-                )
-                datapoints.create(test_datapoint)
+            actual = graph(1, 'date', 'Food')
+            expected = True
+            self.assertEqual(expected, actual)
